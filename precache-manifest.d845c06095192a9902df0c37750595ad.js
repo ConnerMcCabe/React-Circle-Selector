49d0185bad412ebd7e7b88616d6ff9d0fe50beec
Admin <conner.p.mccabe@gmail.com>
2019-10-09 11:54:40 -0500

    Updates

---
+++ precache-manifest.d845c06095192a9902df0c37750595ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e70c4508ce02d0852d7d5f633e64eba6",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "d7e64ea7886534932297",
+    "url": "/project-4-assessment/static/css/main.ad1c7886.chunk.css"
+  },
+  {
+    "revision": "6daea1c74ee858a59fa5",
+    "url": "/project-4-assessment/static/js/2.5aba351a.chunk.js"
+  },
+  {
+    "revision": "d7e64ea7886534932297",
+    "url": "/project-4-assessment/static/js/main.2111d045.chunk.js"
+  },
+  {
+    "revision": "eafed06571e7849b3aa2",
+    "url": "/project-4-assessment/static/js/runtime-main.86c30a1d.js"
+  }
+]);
